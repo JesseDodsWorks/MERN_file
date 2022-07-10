c56a2da5bf4971a0e4bcc959b24ad6423fbacf62
Jesse Dods <ghostling425@gmail.com>
2022-07-10 01:15:22 -0700

    created a react page with JSX

---
+++ react_with_jsx/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Dojo!</h1>
+      <h3>Things I need to do</h3>
+
+      <ul>
+        <li>go to work</li>
+        <li>go to work</li>
+        <li>go to work</li>
+        <li>go to work</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
