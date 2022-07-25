7dbdd9071b3c027eb1fce02b554f56f7431a1d6a
Jesse Dods <ghostling425@gmail.com>
2022-07-24 21:42:25 -0700

    working todo list

--- react/list_todo/src/App.js
+++ react/list_todo/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
+import Form from "./components/Form";
+import Display from "./components/Display";
 
 function App() {
+
+  const [todoList, setTodoList] = useState([]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Add a task</h3>
+      <Form todoList={todoList} setTodoList={setTodoList} />
+      <hr></hr>
+      <h3>List of todos</h3>
+      <Display todoList={todoList} setTodoList={setTodoList} />
     </div>
   );
 }
