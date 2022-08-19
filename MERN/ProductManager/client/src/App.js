5d89c0c57e1f818c5b51e3c751556b03d9928ec8
Jesse Dods <ghostling425@gmail.com>
2022-08-18 20:34:35 -0700

    product manager part 1. Can create a product

---
+++ MERN/ProductManager/client/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import axios from 'axios';
+import './App.css';
+
+function App() {
+
+  const [title, setTitle] = useState(""); 
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+    
+    console.log("hello from the other side")
+
+    axios.post('http://localhost:8000/api/products', {
+        title: title, 
+        price: price,
+        description: description,  
+    })
+        .then(res=>{
+            console.log(res); 
+            console.log(res.data);
+        })
+        .catch(err=>console.log(err))
+}
+
+  return (
+    <div className="App">
+      <h3>Product Manager</h3>
+
+      <form onSubmit={onSubmitHandler}> 
+        <input placeholder="item" onChange={ (e) => setTitle(e.target.value) } value={title} />
+        <input placeholder="price" onChange={ (e) => setPrice(e.target.value) } value={price}/>
+        <input placeholder="description" onChange={ (e) => setDescription(e.target.value) } value={description}/>
+        <button type="submit"> create </button>
+      </form>
+      
+    </div>
+  );
+}
+
+export default App;
