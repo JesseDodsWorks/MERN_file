72dbfe9bf6b80d31a00e3c08bd08a346761d18ae
Jesse Dods <ghostling425@gmail.com>
2022-08-20 02:45:39 -0700

    revert fixed build issues

--- MERN/ProductManager/client/src/App.js
+++ MERN/ProductManager/client/src/App.js
@@ -4,40 +4,40 @@ import './App.css';
 
 function App() {
 
-  const [title, setTitle] = useState(""); 
-  const [price, setPrice] = useState(0);
-  const [description, setDescription] = useState("");
+	const [title, setTitle] = useState("");
+	const [price, setPrice] = useState(0);
+	const [description, setDescription] = useState("");
 
-  const onSubmitHandler = (e) => {
-    e.preventDefault();
-    
-    console.log("hello from the other side")
+	const onSubmitHandler = (e) => {
+		e.preventDefault();
 
-    axios.post('http://localhost:8000/api/products', {
-        title: title, 
-        price: price,
-        description: description,  
-    })
-        .then(res=>{
-            console.log(res); 
-            console.log(res.data);
-        })
-        .catch(err=>console.log(err))
-}
+		console.log("hello from the other side")
 
-  return (
-    <div className="App">
-      <h3>Product Manager</h3>
+		axios.post('http://localhost:8000/api/products', {
+			title: title,
+			price: price,
+			description: description,
+		})
+			.then(res => {
+				console.log(res);
+				console.log(res.data);
+			})
+			.catch(err => console.log(err))
+	}
 
-      <form onSubmit={onSubmitHandler}> 
-        <input placeholder="item" onChange={ (e) => setTitle(e.target.value) } value={title} />
-        <input placeholder="price" onChange={ (e) => setPrice(e.target.value) } value={price}/>
-        <input placeholder="description" onChange={ (e) => setDescription(e.target.value) } value={description}/>
-        <button type="submit"> create </button>
-      </form>
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<h3>Product Manager</h3>
+
+			<form onSubmit={onSubmitHandler}>
+				<input placeholder="item" onChange={(e) => setTitle(e.target.value)} value={title} />
+				<input placeholder="price" onChange={(e) => setPrice(e.target.value)} value={price} />
+				<input placeholder="description" onChange={(e) => setDescription(e.target.value)} value={description} />
+				<button type="submit"> create </button>
+			</form>
+
+		</div>
+	);
 }
 
 export default App;
