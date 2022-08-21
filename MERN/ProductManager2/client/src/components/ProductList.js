2841979ab3408b08fcee66fd1c4a3f51b5fc994d
Jesse Dods <ghostling425@gmail.com>
2022-08-20 21:28:03 -0700

    fixed map function to correctly display products

--- MERN/ProductManager2/client/src/components/ProductList.js
+++ MERN/ProductManager2/client/src/components/ProductList.js
@@ -3,11 +3,10 @@ import axios from 'axios';
 import { Link } from 'react-router-dom';
 
 
+
 export default function ProductList(props) {
 
     const [products, setProducts] = useState([])
-	// const [price, setPrice] = props.allState[1];
-	// const [description, setDescription] = props.allState[2];
 
 	useEffect(()=>{
     	axios.get("http://localhost:8000/api/products")
@@ -23,12 +22,11 @@ export default function ProductList(props) {
 	return (
 		<div>
 			<h3>inside ProductList</h3>
-			{/* { // don't forget this is JSX
+			{ // don't forget this is JSX
 				products.map( (name, index) => {
-					return <Link key={index}> {name.title} </Link>
+					return <Link to={"/"} key={index}> {name.title} </Link>
 				})
-			} */}
-			<p> {products[0].title}</p>
+			}
 		</div>
 	)
 }
